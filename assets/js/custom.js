e204aa5b607a359728a1459999a6350de732b1af
Florin Nita <florin_nitzy@yahoo.com>
2024-01-24 17:01:53 +0200

    Removed unused scripts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,31 +24,6 @@ jQuery(function ($) {
 		});
 	} catch (err) { }
 
-	// Engineer Slider JS
-	$('.engineer-slider').owlCarousel({
-		items: 4,
-		loop: true,
-		margin: 15,
-		singleItem: true,
-		nav: false,
-		dots: true,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			},
-			600: {
-				items: 2,
-			},
-			1000: {
-				items: 4,
-			}
-		}
-	});
-
 	// Review Slider JS
 	$('.review-slider').owlCarousel({
 		items: 1,
@@ -64,16 +39,6 @@ jQuery(function ($) {
 			"<i class='bx bx-chevron-left'></i>",
 			"<i class='bx bx-chevron-right'></i>"
 		],
-	});
-
-	// Popup Youtube JS
-	$('.popup-youtube').magnificPopup({
-		disableOn: 320,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: false
 	});
 
 	// Banner Slider JS
@@ -93,68 +58,8 @@ jQuery(function ($) {
 		],
 	});
 
-	// Service Details Slider JS
-	$('.service-details-slider').owlCarousel({
-		items: 1,
-		loop: true,
-		margin: 15,
-		nav: false,
-		dots: true,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-	});
-
 	// Wow jS
 	new WOW().init();
-
-	// Subscribe Form JS
-	$(".newsletter-form").validator().on("submit", function (event) {
-		if (event.isDefaultPrevented()) {
-			// Hande the invalid form...
-			formErrorSub();
-			submitMSGSub(false, "Please enter your email correctly.");
-		} else {
-			// Everything looks good!
-			event.preventDefault();
-		}
-	});
-	function callbackFunction(resp) {
-		if (resp.result === "success") {
-			formSuccessSub();
-		}
-		else {
-			formErrorSub();
-		}
-	}
-	function formSuccessSub() {
-		$(".newsletter-form")[0].reset();
-		submitMSGSub(true, "Thank you for subscribing!");
-		setTimeout(function () {
-			$("#validator-newsletter").addClass('hide');
-		}, 4000)
-	}
-	function formErrorSub() {
-		$(".newsletter-form").addClass("animated shake");
-		setTimeout(function () {
-			$(".newsletter-form").removeClass("animated shake");
-		}, 1000)
-	}
-	function submitMSGSub(valid, msg) {
-		if (valid) {
-			var msgClasses = "validation-success";
-		} else {
-			var msgClasses = "validation-danger";
-		}
-		$("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
-	}
-
-	// AJAX Mail Chimp JS
-	$(".newsletter-form").ajaxChimp({
-		url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-		callback: callbackFunction
-	});
 
 	// Accordion JS
 	$('.accordion > li:eq(0) a').addClass('active').next().slideDown();
@@ -211,36 +116,6 @@ jQuery(function ($) {
 		return false;
 	});
 
-	// Switch Btn
-	// $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
-
 }(jQuery));
-
-/* function to set a given theme/color-scheme
-function setTheme(themeName) {
-	localStorage.setItem('audeck_theme', themeName);
-	document.documentElement.className = themeName;
-}
-
-// function to toggle between light and dark theme
-function toggleTheme() {
-	if (localStorage.getItem('audeck_theme') === 'theme-dark') {
-		setTheme('theme-light');
-	} else {
-		setTheme('theme-dark');
-	}
-}
-
-// Immediately invoked function to set the theme on initial load
-(function () {
-	if (localStorage.getItem('audeck_theme') === 'theme-dark') {
-		setTheme('theme-dark');
-		document.getElementById('slider').checked = false;
-	} else {
-		setTheme('theme-light');
-		document.getElementById('slider').checked = true;
-	}
-})();
-*/
 
 
